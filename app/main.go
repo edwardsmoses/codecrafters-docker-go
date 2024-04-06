f22d3ed548ec0ead20ff1566b9588ddf650a6f48
Edwards Moses <edwardsmoses3@gmail.com>
2024-04-06 21:32:18 +0000

    feat: better way - getting the exitcode from the cmd process state

--- app/main.go
+++ app/main.go
@@ -1,28 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-
-	// Uncomment this block to pass the first stage!
 	"os"
 	"os/exec"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-
-	// Uncomment this block to pass the first stage!
-	//
 
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
-
-	if args[0] == "exit" {
-		exitCode, _ := strconv.Atoi(args[1])
-		os.Exit(exitCode)
-	}
 
 	cmd := exec.Command(command, args...)
 
@@ -32,7 +19,6 @@ func main() {
 	err := cmd.Run()
 
 	if err != nil {
-		fmt.Printf("Err: %v", err)
-		os.Exit(1)
+		os.Exit(cmd.ProcessState.ExitCode())
 	}
 }
