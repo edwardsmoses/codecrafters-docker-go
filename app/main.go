afd579b7816fe7316fb1a39da0a5072f894e552e
Edwards Moses <edwardsmoses3@gmail.com>
2024-04-07 22:19:58 +0000

    feat:attempt at passing fourth stage

--- app/main.go
+++ app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -10,6 +11,9 @@ func main() {
 
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
+
+	syscall.Chroot("/root/my-docker-container-fs")
+	syscall.Chdir("/") // set the working directory inside container
 
 	cmd := exec.Command(command, args...)
 
